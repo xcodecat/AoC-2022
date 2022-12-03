83272ae9920e4d5f2df8fbdea64c95d22a668772
cheetah's cat <79439124+xcodecat@users.noreply.github.com>
2022-12-03 18:10:15 +0100

    feat(01.py): added solution (not final yet)

---
+++ Day1/01.py
@@ -0,0 +1,23 @@
+file = open("Day1/01.txt")
+
+def function():
+    product = [0]
+    i = 0
+    for line in file:
+        if line == "\n":
+            i += 1
+            product.append(0)
+        else:
+            product[i] += int(line)
+    return product
+
+def part1():
+    product = function()
+    print("Part1: ", str(max(product)))
+
+def part2():
+    product = function()
+    print("Part2: ", str(sum(sorted(product, reverse=True)[:3])))
+
+part1()
+part2()
